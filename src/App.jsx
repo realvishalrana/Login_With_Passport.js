858ec7fe5bdc223c0b3fc04b2407a256c8b20b2e
vishal <vrana14693@gmail.com>
2022-01-06 10:55:24 +0530

    post page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import "./app.css";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Post />
     </div>
   );
 }
